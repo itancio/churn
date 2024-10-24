d6bddc606c4132cebeb5c8b7b8578b4d6a8688be
Irvin Tancioco <irvin.tanc@gmail.com>
2024-10-24 11:56:13 -0700

    fix model probabilities display"

--- streamlit/main.py
+++ streamlit/main.py
@@ -1,18 +1,29 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
 import pickle
 import utils as ut
 import sklearn as sk
 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# For debugging purposes
 print('Numpy verstion: ', np.__version__)
 print('Pandas verstion: ', pd.__version__)
 print('Sklearn verstion: ', sk.__version__)
 
-# client = OpenAI(
-#   base_url="https://api.groq.com/openaiv1",
-#   api_key = os.environ.get("OPENAI_API_KEY")
-# )
+load_dotenv()
+openai_key = os.getenv("OPENAI_API_KEY")
+if not openai_key:
+  st.error("Please set the OPENAI_API_KEY environment variable.")
+  exit(1)
+
+client = OpenAI(
+  base_url="https://api.groq.com/openai/v1",
+  api_key = openai_key
+)
 
 
 def load_model(filename):
@@ -95,111 +106,114 @@ def make_predictions(input_df, input_dict):
     st.plotly_chart(fig, use_container_width=True)
     st.write(f"The customer has a {avg_probability:.2%} probability of churning.")
 
+    st.markdown("### Model Probabilities")
+    col1_1, col1_2 = st.columns(2)
+    for model, prob in probabilities.items():
+      col1_1.write(f"{model}: ")
+      col1_2.write(f"{prob * 100:.2f}%")
+
   with col2:
     fig_probs = ut.create_model_probability_chart(probabilities)
     st.plotly_chart(fig_probs, use_container_width=True)
 
-  st.markdown("### Model Probabilities")
-  for model, prob in probabilities.items():
-    st.write(f"{model}: {prob:.2f}")
-    st.write(f"Average Probability: {avg_probability:.2f}")
+  st.markdown(f"### Average Probability: {avg_probability * 100:.2f}%")
 
   return avg_probability
 
-# def explain_prediction(probability, input_dict, surname):
-#   systemPrompt = f"""
-#   You are an expert data scientist at a bank, wehre you specialize in interpreting
-#   and explaining predictions of machine learning models.
+def explain_prediction(probability, input_dict, surname):
+  systemPrompt = f"""
+  You are an expert data scientist at a bank, wehre you specialize in interpreting
+  and explaining predictions of machine learning models.
 
-#   Your machine learning model has predicted that a customer named {surname} has a
-#   {probability * 100}% probablity of churning, based on the information provided below.
+  Your machine learning model has predicted that a customer named {surname} has a
+  {probability * 100}% probablity of churning, based on the information provided below.
 
-#   Here is the customer's information:
-#   {input_dict}
+  Here is the customer's information:
+  {input_dict}
 
-#   Here are the machine learning model's top 10 most important features for predciting churn:
+  Here are the machine learning model's top 10 most important features for predciting churn:
 
 
-#   features	          importance
-#   NumOfProducts	      0.323888
-#   IsActiveMember	    0.164146
-#   Age	                0.109550
-#   Geography_Germany	  0.091373
-#   Balance	            0.052786
-#   Geography_France	  0.046463
-#   Gender_Female  	    0.045283
-#   Geography_Spain	    0.036855
-#   CreditScore	        0.035005
-#   EstimatedSalary	    0.032655
-#   HasCrCard	          0.031940
-#   Tenure	            0.030054
-#   Gender_Male	        0.000000
+  features	          importance
+  NumOfProducts	      0.323888
+  IsActiveMember	    0.164146
+  Age	                0.109550
+  Geography_Germany	  0.091373
+  Balance	            0.052786
+  Geography_France	  0.046463
+  Gender_Female  	    0.045283
+  Geography_Spain	    0.036855
+  CreditScore	        0.035005
+  EstimatedSalary	    0.032655
+  HasCrCard	          0.031940
+  Tenure	            0.030054
+  Gender_Male	        0.000000
 
-#   {pd.set_option('display.max_columns', None)}
+  {pd.set_option('display.max_columns', None)}
 
-#   Here are the summary statistics for churned customers:
-#   {df[df['Exited'] == 1].describe()}
+  Here are the summary statistics for churned customers:
+  {df[df['Exited'] == 1].describe()}
 
-#   Here are summary statistics for non-churned customers:
-#   {df[df['Exited'] == 0].describe()}
+  Here are summary statistics for non-churned customers:
+  {df[df['Exited'] == 0].describe()}
 
-#   - If the customer has over a 40% risk of churning, genearte a 3 sentence explanation of
-#   why they are at risk of churning.
-#   - If the customer has less than a 40$ risk of churning, generate a 3 sentence explanation
-#   of why they might not be at risk of churning.
-#   - Your explanation should be based on the customer's information, the summary statitics
-#   of churned and non-churned customers, and the feature importances provided.
+  - If the customer has over a 40% risk of churning, genearte a 3 sentence explanation of
+  why they are at risk of churning.
+  - If the customer has less than a 40$ risk of churning, generate a 3 sentence explanation
+  of why they might not be at risk of churning.
+  - Your explanation should be based on the customer's information, the summary statitics
+  of churned and non-churned customers, and the feature importances provided.
 
-#   Don't mention the probability of churning, or the machine learning model,
-#   or say anything like "Based on the machine learning model's predictions and top 10 most
-#   important features, just explain the prediction.
+  Don't mention the probability of churning, or the machine learning model,
+  or say anything like "Based on the machine learning model's predictions and top 10 most
+  important features, just explain the prediction.
 
-#   """
+  """
 
-#   print("EXPLANATION PROMPT", systemPrompt)
+  print("EXPLANATION PROMPT", systemPrompt)
 
-#   raw_response = client.chat.completions.create(
-#     model='gpt-4o mini',
-#     messages=[{
-#       'role': 'user',
-#       'content': systemPrompt
-#     }]
-#   )
+  raw_response = client.chat.completions.create(
+    model='gpt-4o mini',
+    messages=[{
+      'role': 'user',
+      'content': systemPrompt
+    }]
+  )
 
-#   return raw_response.choices[0].message.content
+  return raw_response.choices[0].message.content
 
-# def generate_email(probability, input_dict, explanation, surname):
-#   systemPrompt = f"""
-#   You are a manager at HS Bank. You are responsible for ensuring customers stay
-#   with the bank and are incentivized with various offers.
+def generate_email(probability, input_dict, explanation, surname):
+  systemPrompt = f"""
+  You are a manager at HS Bank. You are responsible for ensuring customers stay
+  with the bank and are incentivized with various offers.
 
-#   You noticed a customer named {surname} has a {probability * 100}% probability of churning.
-#   Here is the customer's information:
-#   {input_dict}
+  You noticed a customer named {surname} has a {probability * 100}% probability of churning.
+  Here is the customer's information:
+  {input_dict}
 
-#   Here is the explanation of the customer's churning probability:
-#   {explanation}
+  Here is the explanation of the customer's churning probability:
+  {explanation}
 
-#   Generate an email to the customer based on their information,
-#   asking them to stay if they are at risk of churning, or offerign them incentives
-#   so taht they become more loyal to the bank.
+  Generate an email to the customer based on their information,
+  asking them to stay if they are at risk of churning, or offerign them incentives
+  so taht they become more loyal to the bank.
 
-#   Make sure to list out a set of incentives to stay based on their information,
-#   in bullet point format. Don't ever mention the probability of churning, or
-#   the machine learning model to the customer.
-#   """
+  Make sure to list out a set of incentives to stay based on their information,
+  in bullet point format. Don't ever mention the probability of churning, or
+  the machine learning model to the customer.
+  """
 
-#   raw_response = client.chat.completions. create(
-#     model='gpt-4o mini',
-#     messages=[{
-#       'role': 'user',
-#       'content': systemPrompt
-#     }]
-#   )
+  raw_response = client.chat.completions. create(
+    model='gpt-4o mini',
+    messages=[{
+      'role': 'user',
+      'content': systemPrompt
+    }]
+  )
 
-#   print("\n\nEMAIL PROMPT", systemPrompt)
+  print("\n\nEMAIL PROMPT", systemPrompt)
   
-#   return raw_response.choices[0].message.content
+  return raw_response.choices[0].message.content
 
 
 st.title("Customer Churn Prediction")
