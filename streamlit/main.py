eccdb40959c769d18b0676ef35538bebe753a044
Irvin Tancioco <irvin.tanc@gmail.com>
2024-10-28 02:50:08 -0700

    implement map

--- streamlit/main.py
+++ streamlit/main.py
@@ -348,39 +348,45 @@ with tab2:
   st.title("Fraud Detection Predictions")
 
   # Load dataset
-  path = "https://media.githubusercontent.com/media/itancio/churn/refs/heads/main/notebook/fraud/fraudTrain.csv"
-  df = pd.read_csv(path, index_col=0)
+  # path = "https://media.githubusercontent.com/media/itancio/churn/refs/heads/main/notebook/fraud/fraudTrain.csv"
+  # df = pd.read_csv(path, index_col=0)
+  
+  # Needed for the map
+  # df = df.rename(columns={'long' : 'lon'})
 
-  sample = pd.DataFrame({
-    'cc_num' : 2703186189652095,
-    'merchant' : 'fraud_Rippin, Kub and Mann',
-    'category' : 'misc_net',
-    'amt' : 4.97,
-    'first' : 'Jennifer',
-    'last' : 'Banks',
-    'gender' : 'F',
-    'street' : '561 Perry Cove',
-    'city' : 'Moravian Falls',
+  sample = pd.DataFrame([{
+    'trans_date_trans_time': '2019-01-01 00:00:18', 
+    'cc_num': 2703186189652095, 
+    'merchant': 'fraud_Rippin, Kub and Mann', 
+    'category': 'misc_net', 'amt': 4.97, 
+    'first': 'Jennifer', 
+    'last': 'Banks', 
+    'gender': 'F', 
+    'street': '561 Perry Cove', 
+    'city': 'Moravian Falls', 
+    'state': 'NC', 
+    'zip': 28654, 
+    'lat': 36.0788, 
+    'lon': -81.1781, 
+    'city_pop': 3495, 
+    'job': 'Psychologist, counselling', 
+    'dob': '1988-03-09', 
+    'trans_num': '0b242abb623afc578575680df30655b9', 
+    'unix_time': 1325376018, 
+    'merch_lat': 36.011293, 
+    'merch_long': -82.048315, 
+    'is_fraud': 0
+  }])
+  df = sample
 
-    'trans_num' : '0b242abb623afc578575680df30655b9',
-    'lat' : 36.0788,
-    'long' : -81.1781,
-    'merch_lat' : 36.011293,
-    'merch_long' : -82.048315,
-
-  })
-
-  transactions = [f"{row['trans_num']} - {row['last']}" for _, row in df.iterrows()]
-
+  # transactions = [f"{row['trans_num']} - {row['last']}" for _, row in df.iterrows()]
+  transactions = sample['trans_num'] + ' - ' + sample['last']
   selected_transaction_option = st.selectbox('Select a transaction', transactions)
 
   if selected_transaction_option:
     selected_transaction_id = selected_transaction_option.split(" - ")[0]
     selected_surname = selected_transaction_option.split(" - ")[1]
-    selected_transaction = df.loc[df['trans_num'] == selected_transaction_id].to_dict(
-      orient='records')
-    
-    print(selected_transaction)
+    selected_transaction = df.loc[df['trans_num'] == selected_transaction_id].to_dict(orient='records')
 
     customer_surname = selected_transaction[0]['last']
     customer_gender = selected_transaction[0]['gender']
@@ -388,8 +394,7 @@ with tab2:
     customer_birthdate = selected_transaction[0]['dob']
     customer_address = selected_transaction[0]['street'] + selected_transaction[0]['city'] + selected_transaction[0]['state'] + str(selected_transaction[0]['zip'])
     customer_lat = selected_transaction[0]['lat']
-    customer_long = selected_transaction[0]['long']
-    
+    customer_long = selected_transaction[0]['lon']
 
     selected_city_pop = selected_transaction[0]['city_pop']
     selected_merchant = selected_transaction[0]['merchant']
@@ -402,4 +407,4 @@ with tab2:
     col1, col2 = st.columns(2)
 
     with col1:
-      st.map([customer_lat, customer_long])
+      st.map(selected_transaction, latitude=customer_lat, longitude=customer_long, color="#0044ff", zoom=6,)
